86d1c70aaa0673a4887cdc778daa831edce9ae55
Moon Choi <choimy1991@gmail.com>
2022-04-17 16:14:06 -0700

    oop

--- index.js
+++ index.js
@@ -1,29 +1,77 @@
-let balance = 500.00;
+let balance = 500.0;
 
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
   }
 
-  commit() {
-    balance -= this.amount;
+  get balance() {
+    let balance = 1000;
+    for (let t of this.transactions) {
+      balance += t.value;
+    }
+    return balance;
   }
 
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
 }
 
+class Transaction {
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+  commit() {
+    // Keep track of the time of the transaction
+    this.time = new Date();
+    // Add the transaction to the account
+    this.account.addTransaction(this); //transaction 전체를 기록함 (amount, account, time, transaction type)
+  }
+}
 
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
+  }
 
+  isAllowed() {
+    // deposits always allowed thanks to capitalism.
+    return true;
+  }
+}
 
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
+  }
+
+  isAllowed() {
+    // note how it has access to this.account b/c of parent
+    return this.account.balance - this.amount >= 0;
+  }
+}
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("billybob");
+
+console.log("Starting Balance:", myAccount.balance);
+
+const t1 = new Deposit(200.0, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(400.0, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+const t3 = new Deposit(300.0, myAccount);
+t3.commit();
+
+const t4 = new Withdrawal(100, myAccount);
+t4.commit();
+
+console.log("Ending Balance:", myAccount.balance);
+
+console.log("Account Transaction History: ", myAccount.transactions);
